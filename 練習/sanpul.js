833697b9e38c7dcc09b834b10069bf5d4c108ad8
ozawa takato <m011707039@edu.teu.ac.jp>
2018-10-03 18:12:40 +0900

    ホーム

---
+++ 練習/sanpul.js
@@ -0,0 +1,19 @@
+console.log("ガチャを回すときは");
+console.log("夜中の3時とかがいいらしいとか");
+console.log("そういう都市伝説があるらしい");
+
+let variableA;//宣言
+variableA ="最初の代入A"//代入
+console.log(variableA);//参照
+
+//宣言と代入を同時にできる
+let variableB = "最初の代入B";
+console.log(variableB);
+
+//letで宣言した変数の中身は代入で上書きできる
+variableA ="二目の代入A";
+console.log(variableA);
+
+//変数に別の変数の値を代入することもできる
+variableA = variableB;
+console.log(variableB);
