6f9d85b9f9426cd7497c6136e84c1b6e76c9167c
ozawa takato <m011707039@edu.teu.ac.jp>
2018-10-17 18:31:36 +0900

    ホーム

---
+++ 練習/2018/10/modal.js
@@ -0,0 +1,34 @@
+function showModal() {
+ document.querySelector("#modal").innerHTML ="押された！！"
+} {
+
+}
+
+function changeRed() {
+  document.querySelector("#modal").style.background="Red";
+
+}
+
+function changeYellow() {
+  document.querySelector("#modal").style.background="Yellow";
+
+}
+
+function changeGreen() {
+  document.querySelector("#modal").style.background="Green";
+
+}function appendCircle() {
+  document.querySelector("#modal").append("〇");
+
+}
+
+function appendCross() {
+  document.querySelector("#modal").append("✕")
+
+}
+
+function plusOne() {
+  document.querysSelector("#modal").innerHTML =
+    Number(document.queryselector("#modal").innerHTML) + 1;
+}
+
