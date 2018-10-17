1cc7eb3b57c02290fe2f9a4fea559dc8b8ce9495
ozawa takato <m011707039@edu.teu.ac.jp>
2018-10-18 00:02:09 +0900

    ホーム

--- 練習/2018/10/sanpul.js
+++ 練習/2018/10/sanpul.js
@@ -22,3 +22,38 @@ console.log(-123,0,-321,0xff);
 console.log(-0.1,.123,-3,.0);
 
 
+let anpanmn;
+anpanmn = "アンパンマン";
+console.log(anpanmn);
+
+let baikinman;
+baikinman= "バイキンマン";
+console.log(baikinman);
+
+anpanmn= "新しい顔";
+console.log(anpanmn);
+
+anpanmn=baikinman;
+console.log(anpanmn);
+
+console.log(-123,0,-321,0xff);
+console.log(-0.1,.123,-3,.0);
+console.log("てんさい");
+
+console.log("hogehoge","ほげほげ");
+console.log('piyopiyo'+'ぴよぴよ');
+
+console.log(123+456);
+
+let numA = 10;
+let numB = 20;
+let numC = numA+numB;
+
+console.log(numA);
+console.log(numB);
+console.log(numC);
+
+
+
+
+
