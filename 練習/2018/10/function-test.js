3d0dc271f059e714d918d0406674323d27a341f0
ozawa takato <m011707039@edu.teu.ac.jp>
2018-10-22 15:06:39 +0900

    ホーム

--- 練習/2018/10/function-test.js
+++ 練習/2018/10/function-test.js
@@ -6,9 +6,7 @@ function hogehoge() {
   console.log("ほげほげ");
 }
 
-function 関数名1(仮引数1) {
-  console.log('仮引数の中身は ${仮引数1} です');
+function 関数名1(test) {
+  console.log('仮引数の中身はです' + test);
 }
 
-関数名1("一回目");
-関数名1("二回目");
