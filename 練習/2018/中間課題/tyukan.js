2c227d5e48b3c87f75293afa59c1c568fef29585
ozawa takato <m011707039@edu.teu.ac.jp>
2018-11-08 09:00:17 +0900

    中間課題

---
+++ 練習/2018/中間課題/tyukan.js
@@ -0,0 +1,9 @@
+let tweetsHTML = "<ul>";
+
+let apeendtweet = function(tweet){
+  tweetsHTML += `<li><b>${tweet.name}</b> ${tweet.message} <i>${tweet.tweetedAt} ${tweet.avatar}</i></il>`
+}
+tweets.forEach(apeendtweet);
+
+tweetsHTML += "</ul>"
+document.querySelector("#answer").innerHTML = tweetsHTML;
