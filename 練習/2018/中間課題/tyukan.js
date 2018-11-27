d36f62c531f11d2ccf0e3fd3d6ce734316bca204
ozawa takato <m011707039@edu.teu.ac.jp>
2018-11-27 14:20:29 +0900

    練習

--- 練習/2018/中間課題/tyukan.js
+++ 練習/2018/中間課題/tyukan.js
@@ -1,11 +1,51 @@
-let tweetsHTML = "<ul>";
 
-let apeendtweet = function(tweet){
-  tweetsHTML += `<li><b>${tweet.name}</b> ${tweet.message} <i>${tweet.tweetedAt} ${tweet.avatar}</i></il>`
+
+function zenbu() {
+  let zenbuHTML = "<ul>";
+
+  let zenbutweet = function(tweets){
+    zenbuHTML += `<li><b>${tweets.name}</b> ${tweets.message} <i>${tweets.tweetedAt} ${tweets.avatar}</i></il>`
+  }
+  tweets.forEach(zenbutweet);
+
+  zenbuHTML += "</ul>"
+  document.querySelector("#answer").innerHTML = zenbuHTML;
 }
-tweets.forEach(apeendtweet);
 
-tweetsHTML += "</ul>"
-document.querySelector("#answer").innerHTML = tweetsHTML;
 
+function saburou() {
+  let saburouHTML = "<ul>";
+
+  let saburoutweet = function (tweets) {
+    saburouHTML += `<li><b>${tweets.name}</b> ${tweets.message} <i>${tweets.tweetedAt} ${tweets.avatar}</i> </li>`
+  }
+  tweets.forEach(saburoutweet);
+
+  saburouHTML +="</ul>"
+  document.querySelector("#answer").innerHTML = saburouHTML;
+}
+
+function zirou() {
+  let zirouHTML = "<ul>";
+
+  let ziroutweet = function (tweets) {
+    zirouHTML += `<li><b>${tweets.name}</b> ${tweets.message} <i>${tweets.tweetedAt} ${tweets.avatar}</i> </li>`
+  }
+  tweets.forEach(ziroutweet);
+
+  zirouHTML +="</ul>"
+  document.querySelector("#answer").innerHTML = zirouHTML;
+}
+
+function tarou() {
+  let tarouHTML = "<ul>";
+
+  let taroutweet = function (tweets) {
+    tarouHTML += `<li><b>${tweets.name}</b> ${tweets.message} <i>${tweets.tweetedAt} <img src="${tweets.avatar} height="100" width="100"  ></i> </li>`
+  }
+  tweets.forEach(taroutweet);
+
+  tarouHTML +="</ul>"
+  document.querySelector("#answer").innerHTML = tarouHTML;
+}
 
