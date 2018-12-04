07eafbb3f4f63e5aaf0e4ef8163c71a208dcf906
ozawa takato <m011707039@edu.teu.ac.jp>
2018-12-04 20:10:28 +0900

    sidjr 出席

--- sidjr/javascript/syusseki.js
+++ sidjr/javascript/syusseki.js
@@ -8,8 +8,16 @@ fetch("https://sheets.googleapis.com/v4/spreadsheets/1tdulfG7Vu0W2eADB_En5dlq1PT
       let html = "<ul>";
 
       json.values.forEach(row => {
-      html += `<li>${row[0]},${row[1]},${row[2]},${row[3]}</li>`;
-      }
+            html += `<hr width="100%" >
+        <table class="table table-bordered">
+          <thead>
+          <tr>
+            <th scope="col">${row[4]}</th>
+            <th scope="col">${row[1]}</th>
+            <th scope="col">${row[2]}</th>
+            <th scope="col">${row[3]}</th>
+          </tr>`;
+          }
       );
 
       html +="</ul>"
