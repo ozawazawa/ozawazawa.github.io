f2731ce9d85abe3c4899f0f20acc71ad14690adf
ozawa takato <m011707039@edu.teu.ac.jp>
2018-12-06 15:49:57 +0900

    sidjr 出席

--- 練習/2018/12/animation.js
+++ 練習/2018/12/animation.js
@@ -1,17 +1,9 @@
 function startAnimation(){
   anime({
     targets: "#animation-box",
-    translateX: 400,
-    translateY: 500
+    translateX: 70,
+    opacity: .5,
+    rotate: 540
   })
 }
 
-var lineDrawing = anime({
-  targets: '#lineDrawing .lines path',
-  strokeDashoffset: [anime.setDashoffset, 0],
-  easing: 'easeInOutSine',
-  duration: 1500,
-  delay: function(el, i) { return i * 250 },
-  direction: 'alternate',
-  loop: true
-});
