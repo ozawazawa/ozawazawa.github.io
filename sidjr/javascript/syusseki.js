7baa9a1858fdfe091d69f34026a109319bc749cf
ozawa takato <m011707039@edu.teu.ac.jp>
2018-12-12 18:26:15 +0900

    sidjr

--- sidjr/javascript/syusseki.js
+++ sidjr/javascript/syusseki.js
@@ -33,6 +33,42 @@ fetch("https://sheets.googleapis.com/v4/spreadsheets/1tdulfG7Vu0W2eADB_En5dlq1PT
     }
 );
 
+function ALL() {
+  fetch("https://sheets.googleapis.com/v4/spreadsheets/1tdulfG7Vu0W2eADB_En5dlq1PTts1a9AMbh9XxviYNo/values/12月出欠確認?key=AIzaSyB5AJs2hAiY6tjNxyH8m6H69eR0S50Fb5g").then(
+      response => {
+        return response.json();
+      }
+  ).then(
+      json => {
+        console.log(json);
+        let html = "<ul>";
+        json.values.forEach(row => {
+                html += `<hr width="100%" >
+        <table class="table table-bordered">
+          <thead>
+             <tr>
+              <th scope="col" style="width:20%">日程</th>
+              <th scope="col" style="width:40%">名前</th>
+              <th scope="col" style="width:10%">出欠</th>
+            　<th scope="col" style="width:20%">到着予想時刻</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>${row[4]}</td>
+              <td>${row[1]}</td>
+              <td>${row[2]}</td>
+              <td>${row[3]}</td>
+            </tr>
+          </tbody>
+        </table>`;
+            }
+        );
+        html += "</ul>";
+        document.querySelector("#result").innerHTML = html;
+      }
+  )
+}
 
 
 
@@ -76,3 +112,80 @@ function syusseki() {
   )
 }
 
+function kesseki() {
+  fetch("https://sheets.googleapis.com/v4/spreadsheets/1tdulfG7Vu0W2eADB_En5dlq1PTts1a9AMbh9XxviYNo/values/12月出欠確認?key=AIzaSyB5AJs2hAiY6tjNxyH8m6H69eR0S50Fb5g").then(
+      response => {
+        return response.json();
+      }
+  ).then(
+      json => {
+        console.log(json);
+        let html = "<ul>";
+        json.values.forEach(row => {
+              if (row[2] == "欠席") {
+                html += `<hr width="100%" >
+        <table class="table table-bordered">
+          <thead>
+             <tr>
+              <th scope="col" style="width:20%">日程</th>
+              <th scope="col" style="width:40%">名前</th>
+              <th scope="col" style="width:10%">出欠</th>
+            　<th scope="col" style="width:20%">到着予想時刻</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>${row[4]}</td>
+              <td>${row[1]}</td>
+              <td>${row[2]}</td>
+              <td>${row[3]}</td>
+            </tr>
+          </tbody>
+        </table>`;
+              }
+            }
+        );
+        html += "</ul>";
+        document.querySelector("#result").innerHTML = html;
+      }
+  )
+}
+
+function tikoku() {
+  fetch("https://sheets.googleapis.com/v4/spreadsheets/1tdulfG7Vu0W2eADB_En5dlq1PTts1a9AMbh9XxviYNo/values/12月出欠確認?key=AIzaSyB5AJs2hAiY6tjNxyH8m6H69eR0S50Fb5g").then(
+      response => {
+        return response.json();
+      }
+  ).then(
+      json => {
+        console.log(json);
+        let html = "<ul>";
+        json.values.forEach(row => {
+              if (row[2] == "遅刻") {
+                html += `<hr width="100%" >
+        <table class="table table-bordered">
+          <thead>
+             <tr>
+              <th scope="col" style="width:20%">日程</th>
+              <th scope="col" style="width:40%">名前</th>
+              <th scope="col" style="width:10%">出欠</th>
+            　<th scope="col" style="width:20%">到着予想時刻</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>${row[4]}</td>
+              <td>${row[1]}</td>
+              <td>${row[2]}</td>
+              <td>${row[3]}</td>
+            </tr>
+          </tbody>
+        </table>`;
+              }
+            }
+        );
+        html += "</ul>";
+        document.querySelector("#result").innerHTML = html;
+      }
+  )
+}
